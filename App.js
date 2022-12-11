b4c5c081d7d8bf479dbcb909505b43359c1ad561
mg143pavankumar <pavansharma.mg143@gmail.com>
2022-12-11 23:14:44 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import React from "react";
+import { colors, parameters } from "./src/global/styles";
+import HomeScreen from "./src/screens/HomeScreen";
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <HomeScreen />
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.white,
+    paddingBottom: 30,
   },
 });
---
+++ src/global/images.js
@@ -0,0 +1,7 @@
+export const images = {
+  uberCarImg: require("../../assets/images/uberCar.png"),
+  foodImg: require("../../assets/images/food.png"),
+  rideImg: require("../../assets/images/ride.png"),
+  packageImg: require("../../assets/images/package.png"),
+  reserveImg: require("../../assets/images/reserve.png"),
+};
